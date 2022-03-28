b0c05c968c1982fd3ee7f72b97e9cf585af38630
Professor Akram <professorakramburris@gmail.com>
2022-03-28 13:50:25 -0500

    Spawning
    Page 566-598

---
+++ SpaceSHMUP-Unity/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,59 @@
+/**** 
+ * Created by: Your Name
+ * Date Created: March 16, 2022
+ * 
+ * Last Edited by: NA
+ * Last Edited: March 16, 2022
+ * 
+ * Description: Spawns eniemes
+****/
+
+/** Using Namespaces **/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    /*** VARIABLES ***/
+    public GameObject[] prefabEnemies;//array of all enemey prefabs
+    public float enemySpawnPerSecond;//enemey count to spawn per second
+    public float enemyDefaultPadding; //padding postition of each enemy
+
+    private BoundsCheck bndCheck; //reference to the bounds check component
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }//end Start()
+
+    void SpawnEnemy()
+    {
+        // Pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);                    
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);    
+
+        // Position the Enemy above the screen with a random x position
+        float enemyPadding = enemyDefaultPadding;                            
+        if (go.GetComponent<BoundsCheck>() != null)
+        {                        // e
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        // Set the initial position for the spawned Enemy                   
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+
+        // Invoke SpawnEnemy() again
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }//end SpawnEnemy()
+}
