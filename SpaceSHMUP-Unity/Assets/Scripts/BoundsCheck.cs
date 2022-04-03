e779ac2dc254d1ec97109848e1f777abbbe8403e
Professor Akram <professorakramburris@gmail.com>
2022-04-03 18:18:58 -0500

    Quick Updates
    Background package imported
    Spelling fix comment - boundscheck

--- SpaceSHMUP-Unity/Assets/Scripts/BoundsCheck.cs
+++ SpaceSHMUP-Unity/Assets/Scripts/BoundsCheck.cs
@@ -38,7 +38,7 @@ public class BoundsCheck : MonoBehaviour
         camHeight = Camera.main.orthographicSize; 
         //Height is the orthographic size of the main camera
         camWidth = camHeight * Camera.main.aspect; 
-        //Aspect ratio is defined in the game view, multipling by the height will give the distance from the orging to the left of right edge of the scene.
+        //Aspect ratio is defined in the game view, multipling by the height will give the distance from the origin to the left of right edge of the scene.
 
     }//end Awake()
 
